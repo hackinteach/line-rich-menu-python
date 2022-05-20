19ef44c35a87a97fcfbce140f54a8560c6aaab51
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2022-05-20 22:04:55 +0700

    feat!: first draft of create command

---
+++ src/line_rich_menu/const.py
@@ -0,0 +1,5 @@
+ACCESS_TOKEN_KEY = "LINE_ACCESS_TOKEN"
+
+NO_TOKEN_TEXT = f"""
+No Channel Access Token provided, specify using --token or environment variable `{ACCESS_TOKEN_KEY}`
+"""
---
+++ src/line_rich_menu/helper.py
@@ -0,0 +1,14 @@
+from typing import Any, Dict
+
+import requests
+
+
+def validate_rich_menu_object(token: str, obj: Dict[str, Any]) -> bool:
+    resp = requests.post(
+        "https://api.line.me/v2/bot/richmenu/validate",
+        json=obj,
+        headers={
+            "Authorization": f"Bearer {token}"
+        }
+    )
+    return resp.status_code == 200
