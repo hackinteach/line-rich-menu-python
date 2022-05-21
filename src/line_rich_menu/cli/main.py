c9174f8a583821af6637436472f172297655ae78
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2022-05-21 11:42:27 +0700

    feat!: only png and jpg image is supported

--- src/line_rich_menu/cli/main.py
+++ src/line_rich_menu/cli/main.py
@@ -7,16 +7,21 @@ import click
 from click import Context
 
 from ..const import ACCESS_TOKEN_KEY, NO_TOKEN_TEXT
-from ..line_rich_menu import LineRichMenu
+from ..menu import LineRichMenu
 
 logger = logging.getLogger("LineRichMenu")
 
 
-@click.group(context_settings={'show_default': True})
-@click.option("-t", "--token", required=False, type=str,
-              envvar=ACCESS_TOKEN_KEY,
-              help=f"Channel access token, can be set via {ACCESS_TOKEN_KEY} environment variable",
-              default=lambda: environ.get(ACCESS_TOKEN_KEY, ""))
+@click.group(context_settings={"show_default": True})
+@click.option(
+    "-t",
+    "--token",
+    required=False,
+    type=str,
+    envvar=ACCESS_TOKEN_KEY,
+    help=f"Channel access token, can be set via {ACCESS_TOKEN_KEY} environment variable",
+    default=lambda: environ.get(ACCESS_TOKEN_KEY, ""),
+)
 @click.pass_context
 def main_cli(ctx: Context, token: Optional[str]):
     if token is None:
@@ -27,25 +32,30 @@ def main_cli(ctx: Context, token: Optional[str]):
 
 
 @main_cli.command()
-@click.option("-d", "--data", type=click.Path(exists=True),
-              help="Rich menu object in json format, see https://developers.line.biz/en/reference/messaging-api/#rich-menu-object")  # noqa
+@click.option(
+    "-d",
+    "--data",
+    type=click.Path(exists=True),
+    help="Rich menu object in json format, see https://developers.line.biz/en/reference/messaging-api/#rich-menu-object",
+)  # noqa
 @click.option("-i", "--image", type=click.Path(exists=True))
 @click.option("--default", type=bool, default=False, help="Set menu as default")
-@click.option("-t", "--token", required=False, type=str,
-              envvar=ACCESS_TOKEN_KEY,
-              help=f"Channel access token, can be set via {ACCESS_TOKEN_KEY} environment variable",
-              default=lambda: environ.get(ACCESS_TOKEN_KEY, ""))
+@click.option(
+    "-t",
+    "--token",
+    required=False,
+    type=str,
+    envvar=ACCESS_TOKEN_KEY,
+    help=f"Channel access token, can be set via {ACCESS_TOKEN_KEY} environment variable",
+    default=lambda: environ.get(ACCESS_TOKEN_KEY, ""),
+)
 @click.pass_context
 def create(ctx: Context, data: str, image: str, default: bool):
     token = ctx.obj.get(ACCESS_TOKEN_KEY)
-    with open(data, 'r') as f:
+    with open(data, "r") as f:
         data_dict = load(f)
     tool = LineRichMenu(token=token)
-    menu_id = tool.create_menu(
-        data=data_dict,
-        image_path=image,
-        set_default=default
-    )
+    menu_id = tool.create_menu(data=data_dict, image_path=image, set_default=default)
     click.echo(menu_id)
 
 
